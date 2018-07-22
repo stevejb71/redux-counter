eee92992b293c99512e20c37cc7beff819dd0073
steve <stevejb71@gmail.com>
2018-07-22 18:49:43 +0800

    Hardcoded counter app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+// @flow strict
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+type State = {
+  +value: number
+}
+
+class App extends Component<State> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Redux counter</h1>
+        <button>increment</button>
+        <button>decrement</button>
+        <div>0</div>
       </div>
     );
   }
